608b3f775649e2e0679aa53394b0976b2d2861b3
Ard-Jan van Etten <ardjanve@gmail.com>
2020-12-12 22:27:42 +0100

    microoptimization

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
-
-
 fn main() {
     for i in 0..=400 {
-
         println!("\"normaal\": {}, mayaans {}", i, naar_ntallig(i, 0, 20).0);
     }
 }
@@ -10,22 +7,20 @@ fn main() {
 fn naar_ntallig (n: u32, macht: u32, radix: u32) -> (String, u32) {
     let mut huidige_x = n;
     let mut huidige_string = "".to_string();
+    let huidigemacht = radix.pow(macht);
 
-    if n >= radix.pow(macht + 1) {
+    if n >= huidigemacht * radix {
         let waarde = naar_ntallig(n, macht + 1, radix);
         huidige_string = waarde.0;
         huidige_x = waarde.1;
     }
 
-    let huidigemacht = radix.pow(macht);
     let aantal = huidige_x / huidigemacht;
     let rest = huidige_x % huidigemacht;
-
     let berekend_symbool = symbool(aantal);
     let complete_tekst_uitvoer = format!("{}{}", huidige_string, berekend_symbool);
 
     (complete_tekst_uitvoer, rest)
-
 }
 
 fn symbool (n: u32) -> String {
